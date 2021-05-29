8a2e8f5d6afd6f94330edd4b3357dd2e85b5e81e
Felipe Tomazelli <fefetl08@gmail.com>
2021-05-29 10:18:52 -0300

    first commit

---
+++ scripts/start_pyproject.ps1
@@ -0,0 +1,13 @@
+$project_name = [System.IO.Path]::GetFileName($pwd)
+
+git init 
+
+gh repo create $project_name --confirm --public --template tomazellifelipe/python-template
+
+git pull origin main
+
+python -m venv .venv
+
+.\.venv\Scripts\Activate.ps1
+
+Remove-Variable project_name
